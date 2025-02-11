c54ab6b63c147ff782fca3f4e7997601b82e18da
unknown <hamida2004dadda@gmail.com>
2025-02-12 00:55:11 +0100

    version1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Home from "./pages/Home";
@@ -7,9 +7,9 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
